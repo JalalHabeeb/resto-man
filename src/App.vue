4eab2df63cc819a12cb100a5f5499fd4c4c1a637
Jalal Habeeb <jalalhabeeb@gmail.com>
2024-03-21 20:27:58 +0100

    add signup component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
+  <SignUp />
+  
 </template>
 
 <script>
+import SignUp from "./components/SignUp.vue";
 export default {
   name: "App",
+  components: {
+    SignUp,
+  },
 };
 </script>
 
@@ -17,4 +22,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,51 @@
+<template>
+  <img class="logo" src="../assets/logo.png"/>
+  <h1>Sign up</h1>
+  <div class="register">
+    <input type="text" placeholder="Enter Name"/>
+    <input type="email" placeholder="Enter Email"/>
+    <input type="password" placeholder="Enter Password"/>
+    <button>Sign Up</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SignUp",
+};
+</script>
+
+<style>
+.logo{
+  width: 100px;
+}
+
+.register input {
+  width: 300 px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin: auto auto 30px auto;
+  border: 1px solid skyblue;
+  border-radius: 5px;
+}
+.register input {
+  width: 300 px;
+  height: 40px;
+  padding-left: 20px;
+  display: block;
+  margin: auto auto 30px auto;
+  border: 1px solid skyblue;
+  border-radius: 5px;
+}
+
+.register button {
+  width: 320px;
+  height: 40px;
+  border: 1px solid skyblue;
+  border-radius: 5px;
+  background-color: skyblue;
+  color: white;
+  cursor: pointer;
+}
+</style>
